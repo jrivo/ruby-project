dee5d9b237f0ed890c7c09932b1910cdb8cf2395
Jonathan Rivo <rivo.jonathan98@gmail.com>
2022-09-13 13:00:56 +0200

    Almost finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "flats", to: "flats#index", as: :flats
+  get "flats/:id", to: "flats#show", as: :flat
+  get "flats/new", to: "flats#new"
+  post "flats", to: "flats#create"
+  get "flats/:id/edit", to: "flats#edit", as: :edit_flat
+  patch "flats/:id", to: "flats#update"
+  delete "flats/:id", to: "flats#destroy"
 
   # Defines the root path route ("/")
   # root "articles#index"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Flat.destroy_all
+flats = Flat.create([{ name: "Flat 1", address: "1 rue de la paix", description: "A nice flat", price_per_night: 100, number_of_guests: 20 },
+                     { name: "Flat 2", address: "2 rue de la liberté", description: "A huge flat", price_per_night: 150, number_of_guests: 60 },
+                     { name: "Flat 3", address: "3 avenue du Charles De Gaulle", description: "A simple flat", price_per_night: 50, number_of_guests: 120 }])
